dda3a7e331730cbb0f9a0b2b28cb27a583591ae8
Juan José <95169921+JJose1987@users.noreply.github.com>
2025-03-25 09:53:29 +0100

    20250325.0953
    20250325.0953

---
+++ functions.js
@@ -0,0 +1,27 @@
+/* JavaScript */
+/* https://www.degraeve.com/reference/urlencoding.php */
+// Variables
+const __Objeto = new Objeto();
+
+// Funciones
+function main() {
+    //Si es telefono girar la vista y las imagenes
+    this.__Objeto = new Objeto();
+    this.__Objeto.sets();
+
+    update();
+}
+
+// Asignar valor del campo
+function set(object) {
+    var index = $(object).attr('name');
+    this.__Objeto.set(index, $(object).val());
+
+    update();
+}
+
+// Actualizar valores de la clase
+function update() {
+
+}
+
