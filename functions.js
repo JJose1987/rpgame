57c461eca8a019164b3d7cabd4dd50fe6652de42
Juan José <95169921+JJose1987@users.noreply.github.com>
2025-04-30 16:21:01 +0200

    20250430.1620
    20250430.1620

--- functions.js
+++ functions.js
@@ -1,19 +1,79 @@
 /* JavaScript */
 /* https://www.degraeve.com/reference/urlencoding.php */
 /*
-    Imagen : "Un ## en estilo pixel art, [descripción]. El diseño es detallado con sombras y colores vibrantes, en un fondo blanco, en un estilo de juego retro similar a los RPG clásicos de 16 bits."
+    Imagen : "En estilo pixel art, [animal] zombie, agresivo, con una runa roja dibujada encima de los ojos, ojos verdes, el color del pelo muy oscuro y color de la piel muy clara un pergamino atado en una de sus piernas, [descripción]. El diseño es detallado con sombras y colores vibrantes, en un fondo blanco, en un estilo de juego retro similar a los RPG clásicos de 16 bits.
     Cartas 2 caracteristicas: N Dados de ataque o defensa, N Usos, 4 distancia magia, 3 distancia arco
 
     Dientes  Comprar armas y provisiones
+
+    Valores (1 a 6)
+        1     : Pifia   – -1
+        2-3   : Fallo   – +0
+        4-5   : Acierto – +1
+        6     : Crítico – +2
+
+///
+cartas imagen
+Imagen de estilo pixel art que representa un pergamino antiguo de color beige claro con bordes enrollados en la parte superior e inferior. En el centro del pergamino habra dibujado [], rodeada de un aura. En la esquina inferior derecha del interior de dentro del pergamino por encima del enrollado inferior, se encuentra dibujado un dado rojo de seis caras con puntos blancos en vista isometrica. El fondo es completamente blanco.
+
+cartas posibles al buscar (3d6)
+(1ºd6)
+    Nada
+    Vida
+    Invoca (dos ojos verdes brillantes con una runa roja entre ambos)
+    Espada (5ºd6 para indicar si => [3 (1 manos) 3+ (2 manos)])
+    Arco
+    Escudo
+
+2ºd6
+    Metal	        Dureza (Mohs)	Estado a Temperatura Ambiente (25 °C)
+    Indio (In)	    1.2	            Sólido (se raya con la uña)
+    Plata (Ag)	    2.5–3	        Sólido
+    Cobre (Cu)	    3	            Sólido
+    Hierro (Fe)	    4	            Sólido
+    Titanio (Ti)	6	            Sólido
+    Cromo (Cr)	    8.5	            Sólido (uno de los más duros)
+
+3ºd6
+    Nada
+    Veneno
+    Fuego
+    Electricidad
+    Radio
+    Explosion
+
+magia es igual a 1ºd6 = 1, 2ºd6 = x, 3ºd6 > 1
+
+
+4d6 el zombie es resultado de sumar todos los dados
+    04, araña
+    05, murcielago
+    06, cuervo
+    07, rata
+    08, lobo
+    09, normal
+    10, arquero
+    11, gordo
+    12, musculoso
+    13, nigromante
+    14, caballo
+    15, caballo espada
+    16, caballo nigromante
+    17, elefante
+    18, elefante espada
+    19, elefante nigromante
+    20, dragon desnudo
+    21, dragon nigromante
+    22, dragon armado
+    23, dios nigromante
+    24, dios armado
 */
 // Variables
 var cnv;
 var ctx;
 
-var kwargs_p = {color : '#000000', x : 0, y : 0, size : 0, sides : 1, fill : true};
-
-
-var colors = ['#89b83f', '#eacb5a', '#36481c', '#eeda43', '#d49d4c', '#bdb9ae']
+var h = $(window).height() * 0.965;
+var w = $(window).width()  * 0.975;
 
 // Funciones
 function main() {
@@ -23,15 +83,18 @@ function main() {
     // Eventos
     // Código a ejecutar cuando la ventana cambia de tamaño
     $(window).resize(function() {
+        h = $(window).height() * 0.965;
+        w = $(window).width()  * 0.975;
+
         $('canvas')
-            .attr('height', $(window).height() * 0.965)
-            .attr('width' , $(window).width()  * 0.975);
+            .attr('height', h)
+            .attr('width' , w);
     });
 
     //
     $('canvas')
-        .attr('height', height * 0.965)
-        .attr('width' , width  * 0.975);
+        .attr('height', h)
+        .attr('width' , w);
     update();
 }
 
@@ -39,8 +102,23 @@ function main() {
 function update() {
     table();
 }
+// Invertir un color dado
+function InversoColor(hex) {
+    return rgbToHex((255 - hexToRgb(hex)['r']), (255 - hexToRgb(hex)['g']), (255 - hexToRgb(hex)['b']));
+}
 
 // Dibujar tablero
 function table() {
-    
+    var size = h / 12;
+
+    var lado = size * (Math.PI / 3);
+    var karg = {colorLine : '#fffffff', size : size, sides : 6, fill : false, rotate : (90 / 1.5), line : 1};
+
+    for (var x = 0; x <= 19; x++) {
+        for (var y = 0; y <= 6; y++) {
+            karg['x'] = x * (lado * (1 + 0.42));
+            karg['y'] = y * (lado * (1 + (1 / 1.5))) + (x % 2 == 0?0:lado * (1 / 1.2));
+            drawPoligon(ctx, karg);
+        }   
+    }
 }
